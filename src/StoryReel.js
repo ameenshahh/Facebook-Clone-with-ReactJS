66be816cd63a0628a889ac768d128c9a9dc9baa8
Ameen Shah <ameenshahshajahan@gmail.com>
2021-01-14 20:49:52 +0530

    completed story reels

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import Story from './Story'
 import './StoryReel.css'
 
 function StoryReel() {
     return (
         <div className="storyReel">
+            <Story
+                image="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-9/12548915_1669514753338025_8941572119595348717_n.jpg?_nc_cat=101&ccb=2&_nc_sid=174925&_nc_ohc=PqMIhEb1G6AAX-1fboo&_nc_ht=scontent.fcok10-1.fna&oh=c046b56d0237514783aa50af0c57f4a8&oe=6026078D"
+                profileSrc="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/44484220_2211069829175743_5472295940108845056_n.jpg?_nc_cat=107&ccb=2&_nc_sid=7206a8&_nc_ohc=dUZc9WjeaOYAX-hONGA&_nc_ht=scontent.fcok10-1.fna&tp=27&oh=c30cae59d48b9e11865982f619ccb5aa&oe=60269E52"
+                title="A"
+            />
 
+            <Story
+                image="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-9/12548915_1669514753338025_8941572119595348717_n.jpg?_nc_cat=101&ccb=2&_nc_sid=174925&_nc_ohc=PqMIhEb1G6AAX-1fboo&_nc_ht=scontent.fcok10-1.fna&oh=c046b56d0237514783aa50af0c57f4a8&oe=6026078D"
+                profileSrc="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/44484220_2211069829175743_5472295940108845056_n.jpg?_nc_cat=107&ccb=2&_nc_sid=7206a8&_nc_ohc=dUZc9WjeaOYAX-hONGA&_nc_ht=scontent.fcok10-1.fna&tp=27&oh=c30cae59d48b9e11865982f619ccb5aa&oe=60269E52"
+                title="B"
+            />
+
+            <Story
+                image="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-9/12548915_1669514753338025_8941572119595348717_n.jpg?_nc_cat=101&ccb=2&_nc_sid=174925&_nc_ohc=PqMIhEb1G6AAX-1fboo&_nc_ht=scontent.fcok10-1.fna&oh=c046b56d0237514783aa50af0c57f4a8&oe=6026078D"
+                profileSrc="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/44484220_2211069829175743_5472295940108845056_n.jpg?_nc_cat=107&ccb=2&_nc_sid=7206a8&_nc_ohc=dUZc9WjeaOYAX-hONGA&_nc_ht=scontent.fcok10-1.fna&tp=27&oh=c30cae59d48b9e11865982f619ccb5aa&oe=60269E52"
+                title="C"
+            />
+
+            <Story
+                image="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-9/12548915_1669514753338025_8941572119595348717_n.jpg?_nc_cat=101&ccb=2&_nc_sid=174925&_nc_ohc=PqMIhEb1G6AAX-1fboo&_nc_ht=scontent.fcok10-1.fna&oh=c046b56d0237514783aa50af0c57f4a8&oe=6026078D"
+                profileSrc="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/44484220_2211069829175743_5472295940108845056_n.jpg?_nc_cat=107&ccb=2&_nc_sid=7206a8&_nc_ohc=dUZc9WjeaOYAX-hONGA&_nc_ht=scontent.fcok10-1.fna&tp=27&oh=c30cae59d48b9e11865982f619ccb5aa&oe=60269E52"
+                title="D"
+            />
+
+            <Story
+                image="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-9/12548915_1669514753338025_8941572119595348717_n.jpg?_nc_cat=101&ccb=2&_nc_sid=174925&_nc_ohc=PqMIhEb1G6AAX-1fboo&_nc_ht=scontent.fcok10-1.fna&oh=c046b56d0237514783aa50af0c57f4a8&oe=6026078D"
+                profileSrc="https://scontent.fcok10-1.fna.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/44484220_2211069829175743_5472295940108845056_n.jpg?_nc_cat=107&ccb=2&_nc_sid=7206a8&_nc_ohc=dUZc9WjeaOYAX-hONGA&_nc_ht=scontent.fcok10-1.fna&tp=27&oh=c30cae59d48b9e11865982f619ccb5aa&oe=60269E52"
+                title="E"
+            />
         </ div>
     )
 }
