54a308d2a70f39724ef9555db0f35e4b56c867d5
Ameen Shah <ameenshahshajahan@gmail.com>
2021-01-15 19:01:01 +0530

    completed the front end

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Widgets from './Widgets';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
+        <Widgets />
       </div>
     </div>
   );
--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import './Feed.css'
 import MessageSender from './MessageSender'
+import Post from './Post'
 import StoryReel from './StoryReel'
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
+
             <MessageSender />
+
+            <Post
+                profilePic='this is a profilepic'
+                message='WOW this works!'
+                timestamp='This is a timestamp'
+                username='ameenshah'
+                image='This is an image'
+            />
+
+            <Post
+                profilePic='this is a profilepic'
+                message='WOW this works!'
+                timestamp='This is a timestamp'
+                username='ameenshah'
+                image='This is an image'
+            />
+            <Post />
         </ div>
     )
 }
